5ac265fb9c2daf36388bfb07112e24b1b234d55d
evapatel11 <evapatel2468@gmail.com>
2023-03-20 12:15:27 +0530

    a

---
+++ tests/example.spec.js
@@ -0,0 +1,39 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects the URL to contain intro.
+  await expect(page).toHaveURL(/.*intro/);
+});
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="password"]').press('Enter');
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await page.locator('a').filter({ hasText: '1' }).click();
+  await page.getByRole('link', { name: 'Sauce Labs Backpack' }).click();
+  await page.locator('[data-test="back-to-products"]').click();
+  await page.locator('a').filter({ hasText: '1' }).click();
+  await page.getByRole('link', { name: 'Sauce Labs Backpack' }).click();
+  await page.locator('[data-test="back-to-products"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+  await page.locator('a').filter({ hasText: '2' }).click();
+  await page.getByRole('link', { name: 'Sauce Labs Bike Light' }).click();
+});
+
